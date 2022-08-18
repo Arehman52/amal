e1499e97ea707ee540bbe4cfa99bf9fee4e8a3bd
Abdur Rehman <abdurrehman418@gmail.com>
2022-08-18 13:26:14 +0500

    initial components

---
+++ src/app/components/imagging-canvas/imagging-canvas.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-imagging-canvas',
+  templateUrl: './imagging-canvas.component.html',
+  styleUrls: ['./imagging-canvas.component.css']
+})
+export class ImaggingCanvasComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ src/app/components/listing/listing-dashboard.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-listing-dashboard',
+  templateUrl: './listing-dashboard.component.html',
+  styleUrls: ['./listing-dashboard.component.css']
+})
+export class ListingDashboardComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
