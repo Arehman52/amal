091685f21dd6442143581ebc59e36d1f2deae7b1
Abdur Rehman <abdurrehman418@gmail.com>
2022-08-25 16:48:50 +0500

    working code with navigator

--- src/app/pages/imagging/imagging.component.ts
+++ src/app/pages/imagging/imagging.component.ts
@@ -1,6 +1,11 @@
 import { Component, NgZone, OnInit } from '@angular/core';
 
-declare var OpenSeadragon: any;
+import * as OpenSeadragon from 'openseadragon';
+// import { Annotorious } from 'openseadragon-annotations';
+
+
+
+// declare var OpenSeadragon: any;
 
 @Component({
   selector: 'app-imagging',
@@ -11,38 +16,55 @@ export class ImaggingComponent implements OnInit {
 
   viewer: any;
   dibujar = false;
-  lineHeight = 1
+  lineHeight = 1;
+
+  duomo = {
+    Image: {
+      xmlns: "http://schemas.microsoft.com/deepzoom/2008",
+      Url: "//openseadragon.github.io/example-images/duomo/duomo_files/",
+      Format: "jpg",
+      Overlap: "2",
+      TileSize: "256",
+      Size: {
+        Width:  "13920",
+        Height: "10200"
+      }
+    }
+  };
 
   constructor(private ngZone: NgZone) { }
 
   ngOnInit() {
 
 
-    var duomo = {
-      Image: {
-        xmlns: "http://schemas.microsoft.com/deepzoom/2008",
-        Url: "//openseadragon.github.io/example-images/duomo/duomo_files/",
-        Format: "jpg",
-        Overlap: "2",
-        TileSize: "256",
-        Size: {
-          Width:  "13920",
-          Height: "10200"
-        }
-      }
-    };
-
-    this.viewer = OpenSeadragon({
-      id: "openseadragon1",
+    const viewer = OpenSeadragon({
+      id: 'openseadragon1',
+      showNavigator:  true,
       prefixUrl: "https://cdn.jsdelivr.net/npm/openseadragon@2.3/build/openseadragon/images/",
-      tileSources: duomo
-    });
+      tileSources: this.duomo
+  });
+
+  viewer;
+  debugger
+
+  // const annotations = Annotorious({ viewer });
 
 
   }
 
 
+init(){
 
+
+
+
+  this.viewer = OpenSeadragon({
+    id: "openseadragon1",
+    prefixUrl: "https://cdn.jsdelivr.net/npm/openseadragon@2.3/build/openseadragon/images/",
+    tileSources: this.duomo
+  });
+}
 
 
 }
+
